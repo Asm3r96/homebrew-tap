2b70a2c73f773812511f70582ccae5e76fef86f4
Mohammed Reschreiter <asm3r96@gmail.com>
2026-03-09 22:49:25 +0100

    style: satisfy brew audit

--- Formula/semplu.rb
+++ Formula/semplu.rb
@@ -4,7 +4,6 @@ class Semplu < Formula
   url "https://registry.npmjs.org/@asm3r96/semplu/-/semplu-0.1.3.tgz"
   sha256 "5b746610ac8b1632eb04feeefab87829317f7164598d01b8f6be4d2205480cd2"
   license "MIT"
-  version "0.1.3"
 
   depends_on "node"
 
