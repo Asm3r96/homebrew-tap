11ccb3f10cc49d0c2f9a97129cc0306f7f3d5408
Mohammed Reschreiter <asm3r96@gmail.com>
2026-03-09 22:55:08 +0100

    fix: create bin dir before wrapper install

--- Formula/semplu.rb
+++ Formula/semplu.rb
@@ -9,6 +9,7 @@ class Semplu < Formula
 
   def install
     system "npm", "install", *std_npm_args(prefix: libexec)
+    bin.mkpath
     bin.install libexec/"bin/semplu"
     (bin/"semplu").write_env_script libexec/"bin/semplu", SEMPLU_VERSION: version.to_s
   end
